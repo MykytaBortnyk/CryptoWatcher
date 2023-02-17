653652a3c3ee0cc0cd193433007e1dbd77ccca61
Mykyta <210401n@gmail.com>
2023-02-17 21:10:18 +0200

    Created WPF UI project

---
+++ CryptoWathcer.Wpf-Ui.Cleint/Models/AppConfig.cs
@@ -0,0 +1,9 @@
+﻿namespace CryptoWatcher.Wpf_Ui.Cleint.Models
+{
+    public class AppConfig
+    {
+        public string ConfigurationsFolder { get; set; }
+
+        public string AppPropertiesFileName { get; set; }
+    }
+}
---
+++ CryptoWathcer.Wpf-Ui.Cleint/Models/DataColor.cs
@@ -0,0 +1,9 @@
+﻿using System.Windows.Media;
+
+namespace CryptoWatcher.Wpf_Ui.Cleint.Models
+{
+    public struct DataColor
+    {
+        public Brush Color { get; set; }
+    }
+}
